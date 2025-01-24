b5a41adf3fca7b3b1abf194fa94487eac0b8e64f
Ch Srivathsavi <chsrivathsavi07@gmail.com>
2025-01-24 19:05:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_man = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move_forward, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_man.create_car()
+    car_man.move_cars()
+
+    # Detect collision with car
+    for car in car_man.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.goto_start()
+        car_man.level_up()
+        scoreboard.increase_level()
+
+
+
+
+
+screen.exitonclick()
+
